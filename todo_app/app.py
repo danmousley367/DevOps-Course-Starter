182c4d544499321274dc59efb6959e4649e95596
Dan Mousley <dan.mousley@softwire.com>
2024-01-26 11:15:22 +0000

    Create to do list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from todo_app.data.session_items import get_items, add_item
 
 from todo_app.flask_config import Config
 
@@ -6,6 +7,14 @@ app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
 def index():
-    return 'Hello World!'
+    if request.method == 'POST':
+            item_title = request.form['item']
+            add_item(item_title)
+
+            return redirect(url_for('index'))
+
+    items = get_items()
+    return render_template('index.html', items=items)
+
