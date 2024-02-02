a69ac1dc39fd9090f18f46e5ff681b08e20a722b
Dan Mousley <dan.mousley@softwire.com>
2024-02-02 11:32:22 +0000

    Add functionality to mark complete

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import get_items, add_item, get_item, save_item
 
 from todo_app.flask_config import Config
 
@@ -10,8 +10,19 @@ app.config.from_object(Config())
 @app.route('/', methods=('GET', 'POST'))
 def index():
     if request.method == 'POST':
-            item_title = request.form['item']
-            add_item(item_title)
+            if 'item' in request.form.keys():
+                  item_title = request.form['item']
+                  add_item(item_title)
+
+            if 'checkbox' in request.form.keys():
+                  checked_item_id = request.form['checkbox']
+                  checked_item = get_item(checked_item_id)
+
+                  if checked_item['status'] == 'Not Started':
+                        checked_item['status'] = 'Finished'
+                  elif checked_item['status'] == 'Finished':
+                        checked_item['status'] = 'Not Started'
+                  save_item(checked_item)
 
             return redirect(url_for('index'))
 
