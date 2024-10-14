1b5fbd4a64463cc784b9281b9712ac3f43bd0087
Dan Mousley <dan.mousley@softwire.com>
2024-10-14 14:58:32 +0100

    Use CosmosDB instead of Trello board

--- todo_app/data/Item.py
+++ todo_app/data/Item.py
@@ -1,14 +1,14 @@
 class Item:
-    def __init__(self, id, name, list_id):
+    def __init__(self, id, name, status):
         self.id = id
         self.name = name
-        self.list_id = list_id
+        self.status = status
 
     @classmethod
-    def from_trello_card(cls, card):
-        return cls(card['id'], card['name'], card['idList'])
+    def from_db(cls, db_item):
+        return cls(db_item['_id'], db_item['name'], db_item['status'])
     
     def __eq__(self, other):
         return self.id == other.id and \
                self.name == other.name and \
-               self.list_id == other.list_id
+               self.status == other.status
--- todo_app/data/ViewModel.py
+++ todo_app/data/ViewModel.py
@@ -1,23 +1,18 @@
 import os
+from todo_app.data.Status import Status
 
 class ViewModel:
-    def __init__(self, items, done_list_id = None, to_do_list_id = None):
+    def __init__(self, items):
         self._items = items
-        self._done_list_id = done_list_id or os.getenv('TRELLO_DONE_LIST_ID')
-        self._todo_list_id = to_do_list_id or os.getenv('TRELLO_TODO_LIST_ID')
  
     @property
     def items(self):
         return self._items
     
     @property
-    def done_list_id(self):
-        return self._done_list_id
-    
-    @property
     def done_items(self):
-        return [item for item in self._items if item.list_id == self._done_list_id]
+        return [item for item in self._items if item.status == Status.COMPLETE.value]
     
     @property
     def todo_items(self):
-        return [item for item in self._items if item.list_id == self._todo_list_id]
+        return [item for item in self._items if item.status == Status.TODO.value]
